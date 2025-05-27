4d9fb3bc233aa2151e7f509dc54c0681c12f9ad8
Rajesh000730 <rajeshrachit09@gmail.com>
2025-05-27 17:55:28 +0530

    modify authgaurd url

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,15 +3,19 @@ import { CanActivate, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  apiUrl: string = environment.apiBaseUrl
+  endpoint: string = "api/ppequestion" 
+
   constructor(private http: HttpClient, private router: Router) {}
 
   canActivate(): Observable<boolean> {
-    return this.http.get('http://localhost:3000/api/ppequestion', { withCredentials: true }).pipe(
+    return this.http.get(`${this.apiUrl}/${this.endpoint}`, { withCredentials: true }).pipe(
       map(() => true), // If successful → allow
       catchError(() => {
         this.router.navigate(['/login']);
